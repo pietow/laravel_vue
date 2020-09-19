42a27457cd8b5696ac0d0a3daf77924f467119d9
piet <pprotassow@posteo.de>
2020-09-19 23:01:20 +0200

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <input type="text" id="input" v-model="message">
+  <p>{{message}}</p>
+  <ul>
+      <li v-for="name in names" v-text="name"></li>
+  </ul>
+  <input type="name" v-model="newName">
+  <button @click="addName">Add name</button>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+    export default {
+        
+            data: function() {
+                return {
+                    message: 'Hello',
+                    names: ['John', 'Doe', 'Mar', 'Dan', 'Apr'],
+                    newName: ''
+                }
+            },
+            methods: { 
+                addName() {
+                    this.names.push(this.newName),
+                    this.newName = ''
+                }
+                 },
+        mounted() {
+            axios.get('https://httpbin.org/get').then(response => 
+            this.names.push(response.data.origin)),
+            console.log('Component mounted.')
+        }
+        
+    }
+</script>
